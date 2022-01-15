fdd174d5bc35661f8e749dff1cf01039e5e9223b
BigBrownBag <maraykin@gmail.com>
2022-01-15 18:56:59 +0300

    DZ2

---
+++ java-spring-2/src/main/java/com/maraykin/springhw/SpringHwApplication.java
@@ -0,0 +1,12 @@
+package com.ityugaev.springhw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHwApplication.class, args);
+    }
+}
